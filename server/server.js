de77443879b72075f4936f1e243cba0f5a74e655
ttristann <tristangalang12@gmail.com>
2025-05-08 17:44:22 -0700

    changed backend to module

--- server/server.js
+++ server/server.js
@@ -1,17 +1,24 @@
-const express = require('express');
-const cors = require('cors');
+// const express = require('express');
+// const cors = require('cors');
+
+// const app = express();
+
+// app.use(express.json());
+// app.use(cors());
+
+// app.get('/adduser', (req, res) => {
+// //   const { name, email } = req.query;
+// //   console.log(`Name: ${name}, Email: ${email}`);
+// //   res.send('User added');
+//     console.log(req.body);
+//     res.send("Response received");
+// });
+
+// app.listen(4000, () => console.log('Server on localhost 4000'));
+import express from 'express';
 
 const app = express();
 
-app.use(express.json());
-app.use(cors());
-
-app.get('/adduser', (req, res) => {
-//   const { name, email } = req.query;
-//   console.log(`Name: ${name}, Email: ${email}`);
-//   res.send('User added');
-    console.log(req.body);
-    res.send("Response received");
+app.listen(4000, () => {
+  console.log('Server on localhost 4000');
 });
-
-app.listen(4000, () => console.log('Server on localhost 4000'));
