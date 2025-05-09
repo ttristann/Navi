caadce69afb79f7edb499947896028d32a8ee6f0
ttristann <tristangalang12@gmail.com>
2025-05-08 17:13:27 -0700

    testing postman

---
+++ server/server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+app.get('/adduser', (req, res) => {
+//   const { name, email } = req.query;
+//   console.log(`Name: ${name}, Email: ${email}`);
+//   res.send('User added');
+    console.log(req.body);
+    res.send("Response received");
+});
+
+app.listen(4000, () => console.log('Server on localhost 4000'));
